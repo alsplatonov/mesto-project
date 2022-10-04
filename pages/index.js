c9bfaff750117f9fd9183ed118db1b91c95b0f22
alsplatonov <platon1560@gmail.com>
2022-10-04 15:42:29 +0300

    add funcs

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
 const container = document.querySelector('.container');
-
 const editButton = container.querySelector('.profile__edit-button');
 const addButton = container.querySelector('.profile__add-button');
-
-const formElementEdit = document.querySelector('.header-popup');
-const formElementAdd = document.querySelector('.card-popup');
-
+const closeButton = document.querySelectorAll('.popup__close-button');
+const popupImgButton = container.querySelector('.element__img');
+const formElementEdit = document.querySelector('.popup__header');
+const formElementAdd = document.querySelector('.popup__card');
+const formElementImg = document.querySelector('.popup__image');
+const formElement = document.querySelectorAll('.popup');
 const nameInput = formElementEdit.querySelectorAll('.popup__input')[0];
 const jobInput = formElementEdit.querySelectorAll('.popup__input')[1];
-
 const titleInput = formElementAdd.querySelectorAll('.popup__input')[0];
 const linkInput = formElementAdd.querySelectorAll('.popup__input')[1];
-
 const profileTitle = container.querySelector('.profile__title');
 const profileSubtitle = container.querySelector('.profile__subtitle');
-const element = container.querySelector('.elements');
-
-
-const closeButtonEdit = formElementEdit.querySelector('.popup__close-button');
-const closeButtonAdd = formElementAdd.querySelector('.popup__close-button');
-
+const elements = container.querySelector('.elements');
+//массив карточек по-умолчанию
 const initialCards = [
   {
     name: 'Архыз',
@@ -47,87 +42,84 @@ const initialCards = [
   }
 ];
 
+function openEditForm() { //открыть форму редактирования профиля
+  formElementEdit.classList.add('popup_opened');
+  openFormEditAttr = formElementEdit.querySelectorAll('.popup__input');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
 
-function openCloseEditForm() {
-  if (formElementEdit.classList.contains('popup_opened')) {
-    formElementEdit.classList.remove('popup_opened')
-  } else {
-    formElementEdit.classList.add('popup_opened');
-    openFormEditAttr = formElementEdit.querySelectorAll('.popup__input');
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
+function openAddForm() { //открыть форму добавления карточки
+  formElementAdd.classList.add('popup_opened');
+}
+
+//закрыть попап 
+function closePopup() {
+  for (let i = 0; i < formElement.length; i++) {
+    formElement[i].classList.remove('popup_opened');
   }
 }
-
-function openCloseAddForm() {
-  if (formElementAdd.classList.contains('popup_opened')) {
-    formElementAdd.classList.remove('popup_opened')
-  } else {
-    formElementAdd.classList.add('popup_opened');
-  }
-}
-
-
-editButton.addEventListener('click', openCloseEditForm);
-addButton.addEventListener('click', openCloseAddForm);
-
-closeButtonEdit.addEventListener('click', openCloseEditForm);
-closeButtonAdd.addEventListener('click', openCloseAddForm);
-
-
-
-function addElementDefault(elementName, elementLink) {
-  const elementTemplate = document.querySelector('#element').content;
-  const elementElement = elementTemplate.querySelector('.element').cloneNode(true);
-
-  elementElement.querySelector('.element__title').textContent = elementName;
-  elementElement.querySelector('.element__img').setAttribute("src", elementLink);
-  elementElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__like-button_active')
-  });
-  // elementElement.querySelector('.element__img').textContent = elementLink;
-  element.append(elementElement);
-}
-
+//добавление элементов
 function addElement(elementName, elementLink) {
   const elementTemplate = document.querySelector('#element').content;
   const elementElement = elementTemplate.querySelector('.element').cloneNode(true);
 
   elementElement.querySelector('.element__title').textContent = elementName;
   elementElement.querySelector('.element__img').setAttribute("src", elementLink);
+  //поставить/убрать лайк
   elementElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like-button_active')
   });
-  // elementElement.querySelector('.element__img').textContent = elementLink;
-  element.prepend(elementElement);
+  //удалить карточку
+  const deleteButton = elementElement.querySelectorAll('.element__delete-button');
+  for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', function () {
+      const listItem = deleteButton[i].closest('.element');
+      listItem.remove();
+    })
+  };
+  elements.prepend(elementElement);
 }
-
-for (let i = 0; i < initialCards.length; i++) {
-
-  addElementDefault(initialCards[i].name, initialCards[i].link);
-}
-
-function formSubmitHandler(evt) {
+//обработчики форм
+function formSubmitHandlerEdit(evt) {
   evt.preventDefault();
-
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  openCloseEditForm();
+  closePopup();
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElementEdit.addEventListener('submit', formSubmitHandler);
 
 function formSubmitHandlerAdd(evt) {
   evt.preventDefault();
   addElement(titleInput.value, linkInput.value);
-  openCloseAddForm();
+  closePopup();
 }
+
+editButton.addEventListener('click', openEditForm);
+
+addButton.addEventListener('click', openAddForm);
+
+for (let i = 0; i < formElement.length; i++) {
+  closeButton[i].addEventListener('click', closePopup);
+};
+
+//заполняем карточки по-умолчанию
+initialCards.forEach(function (item) {
+  addElement(item.name, item.link);
+});
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
+formElementEdit.addEventListener('submit', formSubmitHandlerEdit);
+
 formElementAdd.addEventListener('submit', formSubmitHandlerAdd);
 
+// добавим обработчик
 
 
+
+// for (let i = 0; i < deleteButton.length; i++) {
+//   deleteButton[i].addEventListener('click', function () {
+//     const listItem = deleteButton[i].closest('.element');
+//     listItem.remove();
+//   })
+// };
