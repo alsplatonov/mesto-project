efab03a2048d61be070258f0cc5f9e14cf6ffa52
alsplatonov <platon1560@gmail.com>
2023-01-06 21:56:50 +0300

    minor fixes

--- utils/validate.js
+++ utils/validate.js
@@ -73,11 +73,13 @@ const enableValidation = (validateSelectors) => {
 };
 
 
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__submit-button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active'
-});
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__submit-button',
+//   inactiveButtonClass: 'popup__submit-button_inactive',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__input-error_active'
+// });
+
+export {enableValidation};
