7064cd1584eb7810a09ab6666ae44d0f33e4468d
alsplatonov <platon1560@gmail.com>
2023-01-31 01:57:13 +0300

    change _ to #

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,16 @@
 export default class Section {
   constructor({ items, renderer }, containerSelector) {
-    this._renderedItems = items ;
-    this._renderer = renderer; //функция, которая отвечает за создание и отрисовку данных на странице
+    this.#renderedItems = items ;
+    this.#renderer = renderer; //функция, которая отвечает за создание и отрисовку данных на странице
     
-    this._container = document.querySelector(containerSelector);
+    this.#container = document.querySelector(containerSelector);
   }
 
   renderItems() {
-    this._renderedItems.forEach(item => this._renderer(item))
+    this.#renderedItems.forEach(item => this.#renderer(item))
   }
 
   addItem(element) {
-    this._container.append(element);
+    this.#container.append(element);
   }
 }
